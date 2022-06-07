8406f0ffb71cf3361ddc096cf7b7ca4756b0cb2d
Kemps Medeiros <kempsmm@gmail.com>
2022-06-06 21:24:35 -0300

    created modal and fake database with mirajejs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="kemps budget" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova Transacao
                 </button>
+                
             </Content>
         </Container>
 
